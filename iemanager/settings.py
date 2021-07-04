f73b53f7b47eb15d183fe105d8aef36a7f8157af
ArtusU <artusrock@hotmail.com>
2021-07-04 10:54:51 +0100

    authentication templates

--- iemanager/settings.py
+++ iemanager/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 # from decouple import config
 import environ
 
@@ -116,8 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
