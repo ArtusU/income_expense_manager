44c1d1327909cc9c21fc665f74b5058f3f15410f
ArtusU <artusrock@hotmail.com>
2021-07-04 14:12:34 +0100

    username email validation

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,37 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.views import View
+
+from django.core.validators import validate_email
+
+import json
+
+
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+        try: 
+            validate_email(email)
+        except:
+            return JsonResponse({'email_error': 'Email is invalid'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'sorry email in use, choose another one '}, status=409)
+        return JsonResponse({'email_valid': True})
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'sorry username in use,choose another one '}, status=409)
+        return JsonResponse({'username_valid': True})
 
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
+
