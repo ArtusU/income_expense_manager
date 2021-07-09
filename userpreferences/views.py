6413fd59c0d21ad6abb7658ca6ecac258b9193fc
ArtusU <artusrock@hotmail.com>
2021-07-09 12:19:01 +0100

    user preffered currency

--- userpreferences/views.py
+++ userpreferences/views.py
@@ -1,7 +1,9 @@
+from genericpath import exists
+from iemanager.settings import BASE_DIR
 import json
 import os
 
-from django.conf import settings
+from django.conf import Settings, settings
 from django.contrib import messages
 from django.shortcuts import render
 
@@ -9,5 +11,26 @@ from .models import UserPreference
 
 
 def index(request):
-    curency_data = []
-    return render(request, 'preferences/index.html')
+    currency_data = []
+    file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+        for k, v in data.items():
+            currency_data.append({'name': k, 'value': v})
+
+    exists = UserPreference.objects.filter(user=request.user).exists()
+    user_preferences = None
+    if exists:
+        user_preferences = UserPreference.objects.get(user=request.user)
+        
+    if request.method == 'GET':
+        return render(request, 'preferences/index.html', {'currencies': currency_data, 'user_preferences': user_preferences})
+    else:
+        currency = request.POST.get('currency')
+        if exists:
+            user_preferences.currency = currency
+            user_preferences.save()
+        else:
+            UserPreference.objects.create(user=request.user, currency=currency)
+        messages.success(request, 'Changes saved')
+        return render(request, 'preferences/index.html', {'currencies': currency_data, 'user_preferences': user_preferences})
