bbf15307dd5f95e14e9f1f7bf1b390d9566f537f
ArtusU <artusrock@hotmail.com>
2021-07-10 10:46:51 +0100

    add expenses view

--- expenses/views.py
+++ expenses/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+
+from .models import Category, Expense
 
 @login_required(login_url='authentication/login')
 def index(request):
     return render(request, 'expenses/index.html')
 
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'values': request.POST
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/add_expense.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST.get('amount')
+        if not amount:
+            messages.error(request, 'Amount is required.')
+            return render(request, 'expenses/add_expense.html', context)
+        description = request.POST.get('description')
+        date = request.POST.get('expense_date')
+        category = request.POST.get('category')
+        if not description:
+            messages.error(request, 'Description is required.')
+            return render(request, 'expenses/add_expense.html', context)
+
+        Expense.objects.create(
+            amount=amount, 
+            description=description, 
+            category=category,
+            owner=request.user,
+            date=date
+        )
+        messages.success(request, 'Expense saved successfully')
+
+        return redirect('expenses')
