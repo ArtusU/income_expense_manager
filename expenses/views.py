bca4d04e423b3681bb38c744bfc98c3b8d602d42
ArtusU <artusrock@hotmail.com>
2021-07-10 12:56:22 +0100

    expenses edit view

--- expenses/views.py
+++ expenses/views.py
@@ -1,12 +1,19 @@
 from django.contrib import messages
-from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import redirect, render
 
 from .models import Category, Expense
 
 @login_required(login_url='authentication/login')
 def index(request):
-    return render(request, 'expenses/index.html')
+    categories = Category.objects.all()
+    expenses = Expense.objects.filter(owner=request.user)
+
+    context = {
+        'expenses': expenses,
+    }
+    return render(request, 'expenses/index.html', context)
 
 def add_expense(request):
     categories = Category.objects.all()
@@ -37,5 +44,41 @@ def add_expense(request):
             date=date
         )
         messages.success(request, 'Expense saved successfully')
+
+        return redirect('expenses')
+
+
+def expense_edit(request, id):
+    expense = Expense.objects.get(pk=id)
+    categories = Category.objects.all()
+    context = {
+        'expense': expense,
+        'values': expense,
+        'categories': categories
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/edit-expense.html', context)
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/edit-expense.html', context)
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
+
+        if not description:
+            messages.error(request, 'description is required')
+            return render(request, 'expenses/edit-expense.html', context)
+
+        expense.owner = request.user
+        expense.amount = amount
+        expense. date = date
+        expense.category = category
+        expense.description = description
+
+        expense.save()
+        messages.success(request, 'Expense updated  successfully')
 
         return redirect('expenses')
