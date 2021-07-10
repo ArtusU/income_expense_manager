41f472f37c5ca323e72f70a7085d357f26ad759b
ArtusU <artusrock@hotmail.com>
2021-07-10 13:04:02 +0100

    expense delete view

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, add_expense, expense_edit
+from .views import index, add_expense, expense_edit, delete_expense
 
 
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', index, name='expenses'),
     path('add-expense', add_expense, name='add-expenses'),
     path('edit-expense/<int:id>', expense_edit, name="expense-edit"),
+    path('expense-delete/<int:id>', delete_expense, name="expense-delete"),
 ]
