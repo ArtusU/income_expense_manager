cf8d0900c97f939634e09a6805fc2ebb6ac33c7f
ArtusU <artusrock@hotmail.com>
2021-07-16 11:12:21 +0100

    userincome index (list view)

--- userincome/views.py
+++ userincome/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import json
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Source, UserIncome
+from userpreferences.models import UserPreference
+
+
+
+@login_required(login_url='/authentication/login')
+def index(request):
+    income = UserIncome.objects.filter(owner=request.user)
+    paginator = Paginator(income, 5)
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
+    currency = UserPreference.objects.get(user=request.user).currency
+    context = {
+        'income': income,
+        'page_obj': page_obj,
+        'currency': currency
+    }
+    return render(request, 'income/index.html', context)
